be6a62c9c53e24c6b80393a6e11c966ee1c782a5
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2021-05-22 21:17:57 -0300

    componentes de listagem
    movido:
    	JListaLivros
    
    novo:
    	JListaCategorias
    	JListaClientes
    	JListaExemplares
    	JListaMultas
    	JListaUsuarios

---
+++ src/widget/listas/JListaExemplares.java
@@ -0,0 +1,314 @@
+package widget.listas;
+
+// java util
+import java.util.List;
+
+// java awt
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Color;
+
+// java swing
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.JPanel;
+
+// modelos
+import model.Exemplar;
+
+// widget.dados
+import widget.dados.JDadosExemplar;
+
+public class JListaExemplares extends javax.swing.JPanel {
+    
+    private static final int EXEMPLARES_POR_LINHA = 2;
+    
+    private JPanel _jPanelLinhaExemplarRef;
+    
+    // se está observando seleções
+    private boolean observarSelecao;
+    
+    // observador de seleção (MouseClick event handler)
+    private MouseAdapter observadorSelecao;
+    
+    private JDadosExemplar exemplarSelecionado; // exemplar selecionado 
+    private Border         bordaSalva;          // borda normal do exemplar selecionado
+    private Border         bordaSelecao;        // borda do exemplar selecionado
+    
+    public interface ObservadorSelecao {
+        public void selecao(JDadosExemplar exemplar);
+    };
+    
+    private List<ObservadorSelecao> observadoresSelecao;
+    
+    /**
+     * Creates new form JListaLivros
+     */
+    public JListaExemplares() {
+        initComponents();
+        init();
+    }
+    
+    private void init() {
+        var dim = jPanelExemplares.getPreferredSize();
+        dim.height = 0;
+        jPanelExemplares.setPreferredSize(dim);
+        
+        bordaSelecao = BorderFactory.createLineBorder(Color.orange);
+        
+        observadorSelecao = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent ev) {
+                // DEBUG System.out.println("observador executado");
+                JDadosExemplar exemplar = (JDadosExemplar) bsearchComponente(ev.getPoint());
+                
+                // verifica se um exemplar foi selecionado
+                if (exemplar != null) {
+                    __innerSelecionar(exemplar);
+                }
+            }
+        };
+    }
+    
+    private void __innerSelecionar(JDadosExemplar exemplar) {
+        // reseta a borda do exemplar que estava selecionado
+        if (exemplarSelecionado != null) {
+            exemplarSelecionado.setBorder(bordaSalva);
+        }
+        // salva a borda do exemplar que foi selecionado
+        // e troca sua borda autal pela borda de seleção
+        bordaSalva = exemplar.getBorder();
+        exemplar.setBorder(bordaSelecao);
+
+        // salva o exemplar selecionado
+        exemplarSelecionado = exemplar;
+
+        for (ObservadorSelecao obs : observadoresSelecao) {
+            obs.selecao(exemplarSelecionado);
+        }
+    }
+    
+    public void inserirExemplares(List<Exemplar> exemplares) {
+        var dim = jPanelExemplares.getPreferredSize();
+        
+        int w_gap = 20;
+        int h_gap = 5;
+        
+        // itera de dois em dois se possível e dispõe as categorias lado a lado
+        // (categorias sem par são mostradas na esquerda com um espaço em branco na direita)
+        var iter = exemplares.iterator();
+        JPanel panel = null;
+        if (iter.hasNext()) {
+            if (this._jPanelLinhaExemplarRef != null) {
+                int i = 0;
+                do {
+                    this._jPanelLinhaExemplarRef.add(new JDadosExemplar(iter.next()), i);
+                } while ((--i < EXEMPLARES_POR_LINHA) && iter.hasNext());
+                this._jPanelLinhaExemplarRef = null;
+            }
+        } else {
+            // nada adicionado, retorna para evitar revalidação desnecessaria do swing
+            return;
+        }
+        while (iter.hasNext()) {
+            panel = new JPanel();
+            panel.setLayout(new FlowLayout());
+            
+            // cria um JPanel com duas categorias se possível, ou uma se tiver no fim do loop
+            JDadosExemplar exm0 = new JDadosExemplar(iter.next());
+            panel.add(exm0, 0);
+            Dimension minDim  = exm0.getMinimumSize();
+            Dimension prefDim = exm0.getPreferredSize();
+            for (int i = 1; i < EXEMPLARES_POR_LINHA && iter.hasNext(); ++i) {
+                JDadosExemplar exm1 = new JDadosExemplar(iter.next());
+                panel.add(exm1, i);
+                
+                Dimension tmpMinDim = exm1.getMinimumSize();
+                Dimension tmpPrefDim = exm1.getPreferredSize();
+                
+                minDim.width  += tmpMinDim.width + w_gap;
+                prefDim.width += tmpPrefDim.width + w_gap;
+                
+                minDim.height  =
+                    tmpMinDim.height > minDim.height ?
+                        tmpMinDim.height : minDim.height;
+                prefDim.height = 
+                    tmpPrefDim.height > prefDim.height ?
+                        tmpPrefDim.height : prefDim.height;
+            }
+            
+            // atualiza as dimensões
+            dim.height += prefDim.height + h_gap;
+            dim.width = (prefDim.width > dim.width ? prefDim.width : dim.width);
+            dim.width = (minDim.width > dim.width ? minDim.width : dim.width);
+            
+            // configura o tamanho do panel
+            panel.setMinimumSize(minDim);
+            panel.setPreferredSize(prefDim);
+            
+            // adiciona o panel
+            jPanelExemplares.add(panel);
+        }
+        // atualiza a referencia ao ultimo panel
+        this._jPanelLinhaExemplarRef = panel;
+        
+        jPanelExemplares.setPreferredSize(dim);
+        jPanelExemplares.revalidate();
+    }
+    
+    /**
+     * remove todos os exemplares da lista
+     */
+    public void esvaziar() {
+        jPanelExemplares.removeAll();
+        
+        _jPanelLinhaExemplarRef = null;
+        exemplarSelecionado = null;
+        bordaSalva = null;
+    }
+    
+    /**
+     * retorna o exemplar no indice dado, pode gerar uma exceção
+     * 
+     * @param indice o indice do exmplar
+     * @return o exemplar no dado indice
+     */
+    public JDadosExemplar getExemplar(int indice) {
+        JPanel panel = (JPanel) jPanelExemplares.getComponent(indice / EXEMPLARES_POR_LINHA);
+        return (JDadosExemplar) panel.getComponent(indice % EXEMPLARES_POR_LINHA);
+    }
+    
+    /**
+     * procura o componente na localização dada, retorna null caso não exista
+     * 
+     * implementação: procura binaria da linha, seguido de procura de colisão
+     * 
+     * @param pt localização relativa a jPanelLivros
+     * @return o exmplar na localização
+     */
+    private Component bsearchComponente(Point pt) {
+        int len = jPanelExemplares.getComponentCount();
+        int idx = 0;
+        do {
+            Component comp = jPanelExemplares.getComponent(idx);
+            var pos = comp.getLocation();
+            var dim = comp.getSize();
+            len /= 2;
+            // DEBUG System.out.println("curr line: " + idx);
+            if (pt.y < pos.y) {
+                // falha, procurar na metade de baixo
+                // DEBUG System.out.println("dec");
+                idx -= len;
+            } else if (pt.y >= pos.y + dim.height) {
+                // falha, procurar na metade de cima
+                // DEBUG System.out.println("inc");
+                idx += len;
+            } else {
+                // linha encontrada, retorna o componente dentro da linha que colidir
+                var p = comp.getComponentAt(
+                    new Point(pt.x - pos.x, pt.y - pos.y)
+                );
+                if (p != comp) {
+                    return p;
+                }
+            }
+        } while (len > 0);
+        return null;
+    }
+    
+    public void setObservarSelecao(boolean observarSelecao) {
+        if (this.observarSelecao == observarSelecao) return;
+        
+        if (observarSelecao) {
+            jPanelExemplares.addMouseListener(observadorSelecao);
+        } else {
+            jPanelExemplares.removeMouseListener(observadorSelecao);
+        }
+        this.observarSelecao = observarSelecao;
+    }
+    
+    public boolean getObservarSelecao() {
+        return this.observarSelecao;
+    }
+    
+    public JDadosExemplar getExemplarSelecionado() {
+        return this.exemplarSelecionado;
+    }
+    
+    public void addObservadorSelecao(ObservadorSelecao obs) {
+        observadoresSelecao.add(obs);
+    }
+    public void removeObservadorSelecao(ObservadorSelecao obs) {
+        observadoresSelecao.remove(obs);
+    }
+
+    /**
+     * seleciona o exemplar pelo id do seu exemplar
+     * 
+     * @param id o id do exemplar
+     */
+    public void selecionar(int id) {
+        for (JPanel p : (JPanel[]) jPanelExemplares.getComponents()) {
+            for (JDadosExemplar ex : (JDadosExemplar[]) p.getComponents()) {
+                if (ex.getExemplar().getIdExemplar() == id) {
+                    __innerSelecionar(ex);
+                    return;
+                }
+            }
+        }
+    }
+    
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPaneExemplares = new javax.swing.JScrollPane();
+        jPanelExemplares = new javax.swing.JPanel();
+        jBarraPesquisaSimples1 = new widget.JBarraPesquisaSimples();
+
+        setPreferredSize(new java.awt.Dimension(566, 600));
+
+        jScrollPaneExemplares.setMinimumSize(new java.awt.Dimension(542, 22));
+        jScrollPaneExemplares.setPreferredSize(new java.awt.Dimension(542, 533));
+
+        jPanelExemplares.setPreferredSize(new java.awt.Dimension(524, 530));
+        jPanelExemplares.setLayout(new javax.swing.BoxLayout(jPanelExemplares, javax.swing.BoxLayout.Y_AXIS));
+        jScrollPaneExemplares.setViewportView(jPanelExemplares);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jBarraPesquisaSimples1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPaneExemplares, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jBarraPesquisaSimples1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPaneExemplares, javax.swing.GroupLayout.DEFAULT_SIZE, 527, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private widget.JBarraPesquisaSimples jBarraPesquisaSimples1;
+    private javax.swing.JPanel jPanelExemplares;
+    private javax.swing.JScrollPane jScrollPaneExemplares;
+    // End of variables declaration//GEN-END:variables
+}
--- src/widget/listas/JListaLivros.java
+++ src/widget/listas/JListaLivros.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package widget;
+package widget.listas;
 
 // swing
+import java.awt.Color;
 import javax.swing.JPanel;
 
 // awt
@@ -13,6 +14,9 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 // java util
 import java.util.List;
@@ -20,12 +24,15 @@ import java.util.List;
 // modelos
 import model.Livro;
 
-// graphic.util.livro
+// widget
+import widget.dados.JDadosLivro;
 import widget.support.IComponenteLivro;
 
 // java lang.reflect
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
 
 /**
  *
@@ -35,6 +42,22 @@ public class JListaLivros extends javax.swing.JPanel {
     
     private ComponentAdapter resizeListener;
     
+    // se está observando seleções
+    private boolean observarSelecao;
+    
+    // observador de seleção (MouseClick event handler)
+    private MouseAdapter observadorSelecao;
+    
+    private JPanel livroSelecionado; // livro selecionado 
+    private Border bordaSalva;       // borda normal do livro selecionado
+    private Border bordaSelecao;     // borda do livro selecionado
+    
+    public interface ObservadorSelecao {
+        public void selecao(IComponenteLivro livro);
+    };
+    
+    private List<ObservadorSelecao> observadoresSelecao;
+
     /**
      * Creates new form JListaLivros
      */
@@ -48,11 +71,42 @@ public class JListaLivros extends javax.swing.JPanel {
         dim.height = 0;
         jPanelLivros.setPreferredSize(dim);
         
+        // borda de seleção
+        bordaSelecao = BorderFactory.createLineBorder(Color.orange);
+        
         // observador de redimensinamento
         resizeListener = new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 conteudoRedimensionado();
+            }
+        };
+        
+        // observador de seleção
+        observadorSelecao = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent ev) {
+                // DEBUG System.out.println("observador executado");
+                JPanel panel = (JPanel) bsearchComponente(ev.getPoint());
+                // verifica se um livro foi selecionado
+                if (panel != null) {
+                    // reseta a borda do livro que estava selecionado
+                    if (livroSelecionado != null) {
+                        livroSelecionado.setBorder(bordaSalva);
+                    }
+                    // salva a borda do livro que foi selecionado
+                    // e troca sua borda autal pela borda de seleção
+                    bordaSalva = panel.getBorder();
+                    panel.setBorder(bordaSelecao);
+                    
+                    // salva o exemplar selecionado
+                    livroSelecionado = panel;
+                    
+                    // executa todos os observadores de selecao
+                    for (ObservadorSelecao obs : observadoresSelecao) {
+                        obs.selecao((IComponenteLivro) panel);
+                    }
+                }
             }
         };
     }
@@ -83,6 +137,44 @@ public class JListaLivros extends javax.swing.JPanel {
         jPanelLivros.revalidate();
     }
     
+    /**
+     * procura o componente na localização dada, retorna null caso não exista
+     * 
+     * implementação: procura binaria, seguido de procura de colisão
+     * 
+     * @param pt localização relativa a jPanelLivros
+     * @return o exmplar na localização
+     */
+    private Component bsearchComponente(Point pt) {
+        int len = jPanelLivros.getComponentCount();
+        int idx = 0;
+        do {
+            Component comp = jPanelLivros.getComponent(idx);
+            var pos = comp.getLocation();
+            var dim = comp.getSize();
+            len /= 2;
+            // DEBUG System.out.println("curr line: " + idx);
+            if (pt.y < pos.y) {
+                // falha, procurar na metade de baixo
+                // DEBUG System.out.println("dec");
+                idx -= len;
+            } else if (pt.y >= pos.y + dim.height) {
+                // falha, procurar na metade de cima
+                // DEBUG System.out.println("inc");
+                idx += len;
+            } else {
+                // linha encontrada, retorna o componente dentro da linha que colidir
+                var p = comp.getComponentAt(
+                    new Point(pt.x - pos.x, pt.y - pos.y)
+                );
+                if (p != comp) {
+                    return p;
+                }
+            }
+        } while (len > 0);
+        return null;
+    }
+    
     // força o JPanel dentro do JScrollPane a ter o tamanho necessário
     // para comportar todos os componentes, mesmo que haja redimensionamento
     public void conteudoRedimensionado() {
@@ -97,6 +189,32 @@ public class JListaLivros extends javax.swing.JPanel {
             jPanelLivros.setPreferredSize(dim);
             jPanelLivros.revalidate();
         }
+    }
+    
+    public IComponenteLivro getLivroSelecionado() {
+        return (IComponenteLivro) livroSelecionado;
+    }
+    
+    public void setObservarSelecao(boolean observarSelecao) {
+        if (this.observarSelecao == observarSelecao) return;
+        
+        if (observarSelecao) {
+            jPanelLivros.addMouseListener(observadorSelecao);
+        } else {
+            jPanelLivros.removeMouseListener(observadorSelecao);
+        }
+        this.observarSelecao = observarSelecao;
+    }
+    
+    public boolean getObservarSelecao() {
+        return this.observarSelecao;
+    }
+    
+    public void addObservadorSelecao(ObservadorSelecao obs) {
+        observadoresSelecao.add(obs);
+    }
+    public void removeObservadorSelecao(ObservadorSelecao obs) {
+        observadoresSelecao.remove(obs);
     }
 
     /**
