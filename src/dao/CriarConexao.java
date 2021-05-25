1c93cea054b5551668a559e4c3d925d9bdf746b2
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2021-05-25 17:41:03 -0300

    melhorias na classe CriarConexao do pacote dao

--- src/dao/CriarConexao.java
+++ src/dao/CriarConexao.java
@@ -6,15 +6,22 @@ import java.sql.SQLException;
 
 // Ver se faz uma telinha para o usuário configurar qual BD usar
 public class CriarConexao {
-    final static String dbuser = "postgres";
-    final static String dbpassw = "123456";//kato123
-    final static String dburl = "jdbc:postgresql://localhost:5432/APST";
+    
+    static final String dbtype = "postgresql";
+    static final String dbaddr = "localhost";
+    static final String dbport = "5432";
+    static final String dbuser = "postgres";
+    static final String dbpass = "123456";
+    static final String dbname = "APST";
+    
+    final static String dburl =
+        "jdbc:" + dbtype + "://" + dbaddr + ':' + dbport + '/' + dbname;
+    
     public static Connection getConexao() throws SQLException {
         try {
-            Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(dburl, dbuser, dbpassw);
-        } catch (ClassNotFoundException e) {
-            throw new SQLException(e.getMessage());
+            return DriverManager.getConnection(dburl, dbuser, dbpass);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
